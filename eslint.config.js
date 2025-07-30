e5b165627612a959ce993a15df992e3b1c40d4f9
samasamatour <cv.samasamamajumapan@gmail.com>
2025-07-30 11:32:09 +0700

    Updated package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import next from "@next/eslint-plugin-next";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, next.configs["core-web-vitals"]],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
